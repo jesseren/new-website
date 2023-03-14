6609725e73377d5536893bd95b2ed0727fb3c855
jesseren <jesseren80@gmail.com>
2023-03-13 22:57:11 -0400

    updated resume

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import resume from '../F2022 Resume.pdf'
+import React from "react";
+import { Link } from "react-router-dom";
+import resume from "../W2023 Resume.pdf";
 
 function Navbar() {
-    return(
-        <div style={{zIndex: '3'}} className=''>
-            <Link className='home' to='/'><img src='home.png' alt='home'/></Link>
-            <div className='navbar'>
-                <Link to='/career'>Experience</Link>
-                <Link to='/projects'>Projects</Link>
-                <Link to='/about'>About Me</Link>
-                <a href={resume} target='_blank' rel='noopener noreferrer'>Resume</a>
-            </div>
-        </div>
-            
-    )
+  return (
+    <div style={{ zIndex: "3" }} className="">
+      <Link className="home" to="/">
+        <img src="home.png" alt="home" />
+      </Link>
+      <div className="navbar">
+        <Link to="/career">Experience</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/about">About Me</Link>
+        <a href={resume} target="_blank" rel="noopener noreferrer">
+          Resume
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
