709fea4ba07a9b696b8429bb972e6f6b3ff06e24
jesseren <jesseren80@gmail.com>
2023-11-06 18:15:02 -0500

    Fixed Project Box Loading

--- src/components/projectBox.js
+++ src/components/projectBox.js
@@ -1,25 +1,27 @@
-import {React, useState}  from 'react'
-import ReactCardFlip from 'react-card-flip';
+import { React, useState } from "react";
+import ReactCardFlip from "react-card-flip";
 
 function ProjectBox(props) {
-    const [isFlipped, setIsFlipped] = useState(false)
+  const [isFlipped, setIsFlipped] = useState(false);
 
-    return (
-        <div className='infoBox' onClick={() => setIsFlipped(!isFlipped)}>
-            <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-                <img src={props.imgurl} alt=''/>
-                <div className={'back ' + props.className}>
-                    {props.description.map((description) => 
-                        <p>{description}</p>
-                    )}
-                    {props.custom}
-                    <div className='git'>
-                        <a href={props.github} target='_blank' rel='noopener noreferrer'>Github</a>
-                    </div>
-                </div>
-            </ReactCardFlip>
+  return (
+    <div className="infoBox" onClick={() => setIsFlipped(!isFlipped)}>
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+        <img className={"front " + props.className} src={props.imgurl} alt="" />
+        <div className={"back " + props.className}>
+          {props.description.map((description) => (
+            <p>{description}</p>
+          ))}
+          {props.custom}
+          <div className="git">
+            <a href={props.github} target="_blank" rel="noopener noreferrer">
+              Github
+            </a>
+          </div>
         </div>
-    )
+      </ReactCardFlip>
+    </div>
+  );
 }
 
-export default ProjectBox
+export default ProjectBox;
